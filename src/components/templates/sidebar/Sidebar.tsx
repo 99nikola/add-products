ed208673137521c0e87739e33ee6715a67b6c19b
Nikola <nikola.popovicc99@gmail.com>
2021-11-17 14:58:50 +0100

    link bug fix

--- src/components/templates/sidebar/Sidebar.tsx
+++ src/components/templates/sidebar/Sidebar.tsx
@@ -5,14 +5,12 @@ import { ReactComponent as PlusIcon} from "../../../res/plus.svg";
 const Sidebar = () => {
     return (
         <div className="sidebar">
-            <Link to="/products" className={classes.link}>
-                <div className={classes.products}>
-                    <p>Products</p>
+            <Link to="/products" className={classes.products}>
+                <p>Products</p>
 
-                    <Link to="/products/new">
-                        <PlusIcon className={classes.plusIcon} />
-                    </Link>
-                </div>
+                <Link to="/products/new" className={classes.plus}>
+                    <PlusIcon className={classes.plusIcon} />
+                </Link>
             </Link>
         </div>
     )
