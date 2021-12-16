ca2e89e5d1a927340252b4c30ac76d60c3283952
Nikola <nikola.popovicc99@gmail.com>
2021-12-16 14:18:36 +0100

    unneccesary imports removed

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useMemo, useState } from 'react';
-import { Link, Route, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import Home from './components/pages/Home';
 import ProductForm from './components/pages/ProductForm';
 import Products from './components/pages/Products';
@@ -17,17 +17,17 @@ const App = () => {
 	const [ products, setProducts ] = useState<IProduct[]>(DEFAUTL_PRODUCTS);
 
 	useEffect(() => {
-	const local: IProduct[] = getProducts();
-	if (!local)
-		return;
+		const local = getProducts();
+		if (local === null)
+			return;
 
-	setProducts(local);
+		setProducts(local);
 	}, []);
 
 	const [ 
-	ProductFormPage, 
-	HomePage,
-	UsersPage
+		ProductFormPage, 
+		HomePage,
+		UsersPage
 	] = useMemo(() => [
 		<ProductForm 
 			setProducts={setProducts} 
@@ -37,34 +37,34 @@ const App = () => {
 	], []);
 
 	const ProductsPage = useMemo(() => (
-	<Products 
-		products={products}
-		setProducts={setProducts}
-		/>
+		<Products 
+			products={products}
+			setProducts={setProducts}
+			/>
 	), [products]);
 
 	return (
-	<div className="container">
-		<input id="sidebar" type="checkbox" />
-		<div className="sidebar-container">
-			<Sidebar /> 
-		</div>
-		<Routes>
-			<Route path="/" element={HomePage}/>
-			<Route path="/products/new" element={ProductFormPage}/>
-			<Route path="/products" element={ProductsPage} />
-			<Route path="/users" element={UsersPage} />
-		</Routes>
-		
-		<div className="menu-container">
-			<label htmlFor="sidebar">
-				<MenuMotionIcon 
-					className="menu-icon"
-					/>
-			</label>
-		</div>
+		<div className="container">
+			<input id="sidebar" type="checkbox" />
+			<div className="sidebar-container">
+				<Sidebar /> 
+			</div>
+			<Routes>
+				<Route path="/" element={HomePage}/>
+				<Route path="/products/new" element={ProductFormPage}/>
+				<Route path="/products" element={ProductsPage} />
+				<Route path="/users" element={UsersPage} />
+			</Routes>
 			
-	</div>
+			<div className="menu-container">
+				<label htmlFor="sidebar">
+					<MenuMotionIcon 
+						className="menu-icon"
+						/>
+				</label>
+			</div>
+				
+		</div>
 	);
 }
 
